595b61b7653a8b163d938e6ba94275aa58b104f0
Sander Revenberg <sander@revenberg.info>
2020-06-20 21:09:45 +0200

    .

--- install.sh
+++ install.sh
@@ -24,8 +24,9 @@ if [ ! -f "/home/pi/.pswrd" ]; then
 
     echo "$h ansible_host=$i" >> /home/pi/ansible.log
 
+    mkdir /home/pi/ansible
     echo "[rpi]" > /home/pi/ansible/hosts
-    echo "$i  ansible_connection=ssh ansible_ssh_user=pi" >> ~/ansible/hosts
+    echo "$i  ansible_connection=ssh ansible_ssh_user=pi" >> /home/pi/ansible/hosts
 
     git clone https://github.com/Revenberg/pi-hole.git
 fi
