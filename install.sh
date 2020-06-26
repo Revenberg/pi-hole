188d8ab5f4c7b59e4cdb602c01e1c4d463e0381e
Sander Revenberg <sander@revenberg.info>
2020-06-26 16:50:56 +0200

    .

--- install.sh
+++ install.sh
@@ -19,17 +19,18 @@ if [ ! -f "/home/pi/.pswrd" ]; then
 
     # Configure IP address in "hosts" file. If you have more than one
     # Raspberry Pi, add more lines and enter details
-    i=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
-    h=$(hostname)
-
-    echo "$h ansible_host=$i" >> /home/pi/ansible.log
-
+    
     mkdir /home/pi/ansible
-    echo "[rpi]" > /home/pi/ansible/hosts
-    echo "$i  ansible_connection=ssh ansible_ssh_user=pi" >> /home/pi/ansible/hosts
 
     git clone https://github.com/Revenberg/pi-hole.git
 fi
+
+echo "[rpi]" > /home/pi/ansible/hosts
+
+ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'  | while read line;
+do
+    echo "$line ansible_connection=ssh ansible_ssh_user=pi" >> /home/pi/ansible/hosts
+done
 
 cd ~/pi-hole
 git pull
