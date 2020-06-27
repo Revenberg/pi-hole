fb9150d9b7a185718b4d998b7bb998b63b92c218
Sander Revenberg <sander@revenberg.info>
2020-06-27 12:08:17 +0200

    .

--- install.sh
+++ install.sh
@@ -30,17 +30,23 @@ if [ ! -f "/home/pi/.pswrd" ]; then
     done
 fi
 
-echo "[rpi]" > /home/pi/ansible/hosts
 
 pswrd=$(cat /home/pi/.pswrd)
-echo "---" > /home/pi/ansible/vault.yml
-echo "ansible_become_pass: $pswrd" >> /home/pi/ansible/vault.yml
-ansible-vault encrypt --vault-password-file /home/pi/.pswrd /home/pi/ansible/vault.yml
+
+echo "all:" > /home/pi/ansible/hosts
+echo "  vars:" >> /home/pi/ansible/hosts
+echo "    ansible_connection: ssh" >> /home/pi/ansible/hosts
+echo "    ansible_ssh_user: pi" >> /home/pi/ansible/hosts
+ansible-vault encrypt_string --vault-password-file /home/pi/.pswrd 'rev61272' --name '    ansible_ssh_pass' >> /home/pi/ansible/hosts
+echo "rpi:" >> /home/pi/ansible/hosts
+echo "  hosts:" >> /home/pi/ansible/hosts
+
 
 ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'  | while read line;
 do
-    echo "$line ansible_connection=ssh ansible_ssh_user=pi ansible_ssh_private_key_file=/home/pi/ansible/vault.yml" >> /home/pi/ansible/hosts
-    
+echo "    $line:" >> /home/pi/ansible/hosts
+echo "      ansible_user: pi" >> /home/pi/ansible/hosts
+
 done
 
 #echo /home/pi/ansible/hosts
